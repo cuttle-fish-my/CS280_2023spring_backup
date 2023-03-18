18eaf4492af058080fc92af4b8bc6a1bdde881ca
libn <libn@shanghaitech.edu.cn>
2023-03-18 16:31:13 +0800

    start ConvolutionalNetworks.ipynb

--- CS280-Spring23-Assignment2/Homework2_PartB/cs231n/classifiers/cnn.py
+++ CS280-Spring23-Assignment2/Homework2_PartB/cs231n/classifiers/cnn.py
@@ -18,15 +18,15 @@ class ThreeLayerConvNet(object):
     """
 
     def __init__(
-        self,
-        input_dim=(3, 32, 32),
-        num_filters=32,
-        filter_size=7,
-        hidden_dim=100,
-        num_classes=10,
-        weight_scale=1e-3,
-        reg=0.0,
-        dtype=np.float32,
+            self,
+            input_dim=(3, 32, 32),
+            num_filters=32,
+            filter_size=7,
+            hidden_dim=100,
+            num_classes=10,
+            weight_scale=1e-3,
+            reg=0.0,
+            dtype=np.float32,
     ):
         """
         Initialize a new network.
@@ -63,7 +63,13 @@ class ThreeLayerConvNet(object):
         ############################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        pass
+        self.params["W1"] = np.random.normal(0, weight_scale, (num_filters, input_dim[0], filter_size, filter_size))
+        self.params["b1"] = np.zeros(num_filters)
+        self.params["W2"] = np.random.normal(0, weight_scale,
+                                             (num_filters * input_dim[1] * input_dim[2] // 4, hidden_dim))
+        self.params["b2"] = np.zeros(hidden_dim)
+        self.params["W3"] = np.random.normal(0, weight_scale, (hidden_dim, num_classes))
+        self.params["b3"] = np.zeros(num_classes)
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         ############################################################################
@@ -102,7 +108,9 @@ class ThreeLayerConvNet(object):
         ############################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        pass
+        scores, cache1 = conv_relu_pool_forward(X, W1, b1, conv_param, pool_param)
+        scores, cache2 = affine_relu_forward(scores, W2, b2)
+        scores, cache3 = affine_forward(scores, W3, b3)
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         ############################################################################
@@ -125,7 +133,14 @@ class ThreeLayerConvNet(object):
         ############################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-        pass
+        loss, dout = softmax_loss(scores, y)
+        loss += 0.5 * self.reg * (np.sum(W1 ** 2) + np.sum(W2 ** 2) + np.sum(W3 ** 2))
+        dout, grads["W3"], grads["b3"] = affine_backward(dout, cache3)
+        dout, grads["W2"], grads["b2"] = affine_relu_backward(dout, cache2)
+        dout, grads["W1"], grads["b1"] = conv_relu_pool_backward(dout, cache1)
+        grads["W3"] += self.reg * W3
+        grads["W2"] += self.reg * W2
+        grads["W1"] += self.reg * W1
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         ############################################################################
